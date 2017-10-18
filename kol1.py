9577452b5711a813da76700ca6431201d360d052
Katarzyna Szydlowska <3szydlowska@taurus.fis.agh.edu.pl>
2017-10-18 19:33:02 +0200

    BankSim_v1

--- kol1.py
+++ kol1.py
@@ -11,3 +11,34 @@
 #Do your best, show off with good, clean, well structured code - this is more important than number of features.
 #After you finish, be sure to UPLOAD this (add, commit, push) to the remote repository.
 #Good Luck
+
+#!/usr/bin/python
+
+class Client:
+    def __init__(self, bank, name, cash):
+        self.bank = bank
+        self.name = name
+        self.cash = cash
+
+class Bank:
+    clientCount = 0
+    def __init__(self, name):
+        self.name = name
+
+    def addClient(self,client):
+        #client = Client(self.name, name, cash)
+    
+    def cashInput(client, money):
+        client.cash += money
+
+    def withdrawal(client, money):
+        client.cash -= money
+        
+    def transferFromAToB(clientA, clientB, money):
+        clientA.cash -= money
+        clientA.cash += money
+
+
+myBank = Bank("mBank")
+myBank.addClient(clientA)
+clientA = Client("mBank","Kowalski",3432)
